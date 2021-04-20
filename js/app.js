10dd93fab2ee6213d4f07b006e46ba6a55419835
JasonKarau <Karauski@gmail.com>
2021-04-19 21:41:25 -0500

    more content!

--- js/app.js
+++ js/app.js
@@ -3,4 +3,18 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
-})
+});
+
+const tabs = document.querySelectorAll('.io');
+const hiddenText = document.querySelectorAll('.hidden-subtext');
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (event) => {
+    if (hiddenText[index].style.display == "none") {
+      tab.innerHTML = "&#8593;";
+      hiddenText[index].style.display = "block";
+    } else {
+      hiddenText[index].style.display = "none";
+      tab.innerHTML = "&#8595;";
+  }});
+});
