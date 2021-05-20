e7562a1be02f26b1a416ed12139d69a198ffef67
JasonKarau <Karauski@gmail.com>
2021-05-19 21:29:31 -0500

    Huge update

--- js/app.js
+++ js/app.js
@@ -21,19 +21,14 @@ tabs.forEach((tab, index) => {
 
 // modal
 
-// create references to the modal...
 var modal = document.getElementById('myModal');
-// to all images -- note I'm using a class!
 var images = document.getElementsByClassName('myImages');
-// the image in the modal
 var modalImg = document.getElementById("img01");
-// and the caption in the modal
 var captionText = document.getElementById("caption");
 
-// Go through all of the images with our custom class
+
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
-  // and attach our click listener for this image.
   img.onclick = function(evt) {
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -43,6 +38,22 @@ for (var i = 0; i < images.length; i++) {
 
 var span = document.getElementsByClassName("close")[0];
 
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+
